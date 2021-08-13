340ac0e7025dbbe72957d105a81f497d5d4bbdc1
SeenKid <30565751+SeenKid@users.noreply.github.com>
2021-08-14 00:28:07 +0200

    Update rejoin.lua

--- rejoin.lua
+++ rejoin.lua
@@ -1,4 +1,5 @@
 repeat
-    wait()
-until game:IsLoaded()
-game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,game.JobId)
+    wait()  
+until game:IsLoaded() 
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,game.JobId) 
+--diconnect and reconnect into the same server
